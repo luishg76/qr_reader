6e19cdc70e44b8f153e2fb45d50f598ae5519ea5
Luis Hernández Gómez <ing.luishg76@gmail.com>
2022-09-18 21:58:48 -0500

    salva(18-09-022)_PC

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,7 +6,34 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
      return Scaffold(
-             body: Center(child: Text('Home Screen'))       
+          appBar: AppBar(
+            centerTitle:true,            
+            title: Text('Historial'),
+            actions:[
+              IconButton(
+                onPressed:(){}, 
+                icon:Icon(Icons.delete_outline), 
+              )
+             ],
+            ),           
+            body: Center(child: Text('Home Screen')),  
+            floatingActionButton: FloatingActionButton(      
+                child: Icon(Icons.qr_code_scanner_outlined),
+                onPressed: (){},
+              ),
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+            bottomNavigationBar: BottomNavigationBar(
+              elevation: 0,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.map),
+                  label: 'Mapa'),
+                  BottomNavigationBarItem(
+                  icon: Icon(Icons.compass_calibration),
+                  label: 'Direcciones')
+              ]
+              ),
+
            );
   }
 }
